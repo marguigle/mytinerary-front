5acd5eb1d9426026aec1376e16a68816142b12a2
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2023-08-02 19:03:44 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Footer } from "./components/footer/Footer";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header-main">
+      <h3>My Tinerary</h3>
+      <button className="btn btn-primary">home</button>
+      <button>cities</button>
+      <button>login</button>
+    </div>
+  );
+};
+
+export default Header;
