bcbe03b00b707a1685d3b1df35dbecb222f4cf09
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2023-08-05 19:10:43 -0300

    se maqueta el carrusel

--- src/components/carrusel/Carrucel.jsx
+++ src/components/carrusel/Carrucel.jsx
@@ -1,32 +1,33 @@
 import "./carrucel.css";
 
 const Carrucel = () => {
+  const FotoCiudades = [
+    { id: 0, foto: "../public/img/copenhagen-142711_1280.jpg" },
+    { id: 1, foto: "../public/img/dubai-2650364_1280.jpg" },
+    { id: 2, foto: "../public/img/london-123785_1280.jpg" },
+    { id: 3, foto: "../public/img/munich-1572429_1280.jpg" },
+    {},
+  ];
+
+  /* let indice = 0; */
+  const prev = () => {};
+
+  const next = () => {};
+
   return (
     <>
-      <button>{"<"}</button>
-      <div className="contenedor-carrusel">
-        <img
-          src="../../public/img/paris-100277_1280.jpg"
-          alt=""
-          className="img-carrusel"
-        />
-        <img
-          src="../../public/img/paris-100277_1280.jpg"
-          alt=""
-          className="img-carrusel"
-        />
-        <img
-          src="../../public/img/paris-100277_1280.jpg"
-          alt=""
-          className="img-carrusel"
-        />
-        <img
-          src="../../public/img/paris-100277_1280.jpg"
-          alt=""
-          className="img-carrusel"
-        />
-      </div>
-      <button>{">"}</button>
+      <section className="section-carousel container-flex">
+        <button className="btn-carroucel" onClick={prev}>
+          {"<"}
+        </button>
+        <div className="contenedor-carrusel col-6">
+          <img src={FotoCiudades[0].foto} alt="" className="img-carrusel" />
+          <img src={FotoCiudades[1].foto} alt="" className="img-carrusel" />
+          <img src={FotoCiudades[2].foto} alt="" className="img-carrusel" />
+          <img src={FotoCiudades[3].foto} alt="" className="img-carrusel" />
+        </div>
+        <button onClick={next}>{">"}</button>
+      </section>
     </>
   );
 };
