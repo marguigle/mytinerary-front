a498dd1e29e459a7eb360905616070fde36d3733
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2023-08-06 13:00:32 -0300

    se logra que funciones el carrusel

--- src/components/carrusel/Carrucel.jsx
+++ src/components/carrusel/Carrucel.jsx
@@ -2,27 +2,47 @@ import "./carrucel.css";
 import { useState } from "react";
 
 const Carrucel = () => {
-  let [indice, setIndice] = useState(0);
+  const [indice, setIndice] = useState(0);
 
   const fotoCiudades = [
-    { id: 0, foto: "../public/img/copenhagen-142711_1280.jpg" },
-    { id: 1, foto: "../public/img/dubai-2650364_1280.jpg" },
-    { id: 2, foto: "../public/img/london-123785_1280.jpg" },
-    { id: 3, foto: "../public/img/munich-1572429_1280.jpg" },
-    { id: 4, foto: "../public/img/paris-100277_1280.jpg" },
-    { id: 5, foto: "../public/img/paris-106863_1280.jpg" },
-    { id: 6, foto: "../public/img/pyramid-3084261_1280.jpg" },
-    { id: 7, foto: "../public/img/washington-dc-85531_1280.jpg" },
+    {
+      id: 0,
+      foto1: "../public/img/copenhagen-142711_1280.jpg",
+      foto2: "../public/img/dubai-2650364_1280.jpg",
+      foto3: "../public/img/london-123785_1280.jpg",
+      foto4: "../public/img/san-fransisco-4065321_1280.jpg",
+    },
+
+    {
+      id: 1,
+      foto1: "../public/img/munich-1572429_1280.jpg",
+      foto2: "../public/img/paris-100277_1280.jpg",
+      foto3: "../public/img/imadrid.jpg",
+      foto4: "../public/img/pyramid-3084261_1280.jpg",
+    },
+
+    {
+      id: 2,
+      foto1: "../public/img/washington-dc-85531_1280.jpg",
+      foto2: "../public/img/imadrid.jpg",
+      foto3: "../public/img/portugal-4828134_1280.jpg",
+      foto4: "../public/img/valencia-82132_1280.jpg",
+    },
+    {
+      id: 3,
+      foto1: "../public/img/venice-4756377_1280.jpg",
+      foto2: "../public/img/portugal-4828134_1280.jpg",
+      foto3: "../public/img/venice-2364149_1280.jpg",
+      foto4: "../public/img/venice-4756377_1280.jpg",
+    },
   ];
 
   const prev = () => {
-    indice == 0
-      ? setIndice(fotoCiudades.length - 1)
-      : setIndice((indice = indice - 1));
+    setIndice((indice - 1 + fotoCiudades.length) % fotoCiudades.length);
   };
 
   const next = () => {
-    setIndice((indice = indice + 1));
+    setIndice((indice + 1) % fotoCiudades.length);
   };
 
   return (
@@ -33,13 +53,25 @@ const Carrucel = () => {
         </button>
         <div className="contenedor-carrusel col-6">
           <img
-            src={fotoCiudades[indice].foto}
+            src={fotoCiudades[indice].foto1}
             alt=""
             className="img-carrusel"
           />
-          <img src={fotoCiudades[1].foto} alt="" className="img-carrusel" />
-          <img src={fotoCiudades[2].foto} alt="" className="img-carrusel" />
-          <img src={fotoCiudades[3].foto} alt="" className="img-carrusel" />
+          <img
+            src={fotoCiudades[indice].foto2}
+            alt=""
+            className="img-carrusel"
+          />
+          <img
+            src={fotoCiudades[indice].foto3}
+            alt=""
+            className="img-carrusel"
+          />
+          <img
+            src={fotoCiudades[indice].foto4}
+            alt=""
+            className="img-carrusel"
+          />
         </div>
         <button onClick={next}>{">"}</button>
       </section>
