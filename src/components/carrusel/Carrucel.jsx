08c8b9be3694ff43bae9d51f46235c238bc2b1c3
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2023-08-06 19:31:44 -0300

    se termina array de ciudades

--- src/components/carrusel/Carrucel.jsx
+++ src/components/carrusel/Carrucel.jsx
@@ -8,32 +8,48 @@ const Carrucel = () => {
     {
       id: 0,
       foto1: "../public/img/copenhagen-142711_1280.jpg",
+      nombre1: "Copenhagen Denmark",
       foto2: "../public/img/dubai-2650364_1280.jpg",
+      nombre2: "Dubai, United Arab Emirates",
       foto3: "../public/img/london-123785_1280.jpg",
+      nombre3: "London, England",
       foto4: "../public/img/san-fransisco-4065321_1280.jpg",
+      nombre4: "San Francisco, California",
     },
 
     {
       id: 1,
       foto1: "../public/img/munich-1572429_1280.jpg",
+      nombre1: "Munich, Germany",
       foto2: "../public/img/paris-100277_1280.jpg",
+      nombre2: "Paris, France",
       foto3: "../public/img/imadrid.jpg",
+      nombre3: "Madrid, Spain",
       foto4: "../public/img/pyramid-3084261_1280.jpg",
+      nombre4: "Cairo, Egypt",
     },
 
     {
       id: 2,
       foto1: "../public/img/washington-dc-85531_1280.jpg",
+      nombre1: "Washington, USA",
       foto2: "../public/img/imadrid.jpg",
+      nombre2: "Madrid, Spain",
       foto3: "../public/img/portugal-4828134_1280.jpg",
+      nombre3: "Lisbon, Portugal",
       foto4: "../public/img/valencia-82132_1280.jpg",
+      nombre4: "Valencia, Spain",
     },
     {
       id: 3,
       foto1: "../public/img/venice-4756377_1280.jpg",
+      nombre1: "Venice, Italy",
       foto2: "../public/img/portugal-4828134_1280.jpg",
+      nombre2: "Lisbon, Portugal",
       foto3: "../public/img/venice-2364149_1280.jpg",
-      foto4: "../public/img/venice-4756377_1280.jpg",
+      nombre3: "Venice, Italy",
+      foto4: "../public/img/barcelona1.jpg",
+      nombre4: "Barcelona, Spain",
     },
   ];
 
@@ -55,27 +71,40 @@ const Carrucel = () => {
             {"👈"}
           </button>
           <div className="contenedor-carrusel ">
-            <img
-              src={fotoCiudades[indice].foto1}
-              alt=""
-              className="img-carrusel"
-            />
-            <img
-              src={fotoCiudades[indice].foto2}
-              alt=""
-              className="img-carrusel"
-            />
-            <img
-              src={fotoCiudades[indice].foto3}
-              alt=""
-              className="img-carrusel"
-            />
-            <img
-              src={fotoCiudades[indice].foto4}
-              alt=""
-              className="img-carrusel"
-            />
+            <div className="contenedorImg">
+              <img
+                src={fotoCiudades[indice].foto1}
+                alt="foto ciudad"
+                className="img-carrusel"
+              />
+              <p>{fotoCiudades[indice].nombre1}</p>
+            </div>
+            <div className="contenedorImg">
+              <img
+                src={fotoCiudades[indice].foto2}
+                alt="foto ciudad"
+                className="img-carrusel"
+              />
+              <p>{fotoCiudades[indice].nombre2}</p>
+            </div>
+            <div className="contenedorImg">
+              <img
+                src={fotoCiudades[indice].foto3}
+                alt="foto ciudad"
+                className="img-carrusel"
+              />
+              <p>{fotoCiudades[indice].nombre3}</p>
+            </div>
+            <div className="contenedorImg">
+              <img
+                src={fotoCiudades[indice].foto4}
+                alt="foto ciudad"
+                className="img-carrusel"
+              />
+              <p>{fotoCiudades[indice].nombre4}</p>
+            </div>
           </div>
+
           <button onClick={next}>{"👉"}</button>
         </section>
       </section>
