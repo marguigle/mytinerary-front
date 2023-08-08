ffcdbba0a2815bc7412411e903be8523210db8ee
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2023-08-07 21:56:24 -0300

    se termina responsive de header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,22 +3,26 @@ import "./header.css";
 const Header = () => {
   return (
     <nav className=" navbar">
-      <h1 id="nombreHeader">My Tinerary</h1>
-      <input type="checkbox" className="checkbox" />
-      <i className="icons fi fi-br-menu-burger burger"></i>
-      <i className="icons fi fi-br-cross cross"></i>
+      <div>
+        <h1 id="nombreHeader">My Tinerary</h1>
+      </div>
+      <div>
+        <input type="checkbox" className="checkbox" />
+        <i className="icons fi fi-br-menu-burger burger"></i>
+        <i className="icons fi fi-br-cross cross"></i>
 
-      <ul className="menu">
-        <li>
-          <a href="#">home</a>{" "}
-        </li>
-        <li>
-          <a href="#">cities</a>{" "}
-        </li>
-        <li>
-          <a href="#">login</a>{" "}
-        </li>
-      </ul>
+        <ul className="menu">
+          <li>
+            <a href="#">home</a>{" "}
+          </li>
+          <li>
+            <a href="#">cities</a>{" "}
+          </li>
+          <li>
+            <a href="#">login</a>{" "}
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
