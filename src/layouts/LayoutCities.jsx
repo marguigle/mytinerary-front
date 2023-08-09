a43cf2dd0c97dcaf1e6d7153bf96d7ce26ce6aab
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2023-08-09 18:51:48 -0300

    se importa estilos de layoutMain a LayoutCities

--- src/layouts/LayoutCities.jsx
+++ src/layouts/LayoutCities.jsx
@@ -1,6 +1,6 @@
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
-
+import "./layoutMain.css";
 const LayoutCities = ({ children }) => {
   return (
     <>
