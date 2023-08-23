b9916676e004d465b1b15ce4dcef6f3d9f164079
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2023-08-23 12:36:06 -0300

    se hace que en la pagina cityDetail se muestre el nombre y la imagen de la ciudad seleccionada

--- src/components/cityDetail/CityDetail.jsx
+++ src/components/cityDetail/CityDetail.jsx
@@ -1,8 +1,30 @@
-import "./cityDetail.css";
+/* import "./cityDetail.css";
 
 const CityDetail = () => {
   return (
     <main className="main-city-detali">
+      <img src="public/img/descarga.png" className="img-consttrction" alt="" />
+    </main>
+  );
+};
+
+export default CityDetail;
+ */
+
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
+import "./cityDetail.css";
+
+const CityDetail = () => {
+  const location = useLocation();
+  const { image, name } = queryString.parse(location.search);
+
+  return (
+    <main className="main-city-detali">
+      <div className="city-detail">
+        <h1>{name}</h1>
+        <img src={image} className="city-image" alt={name} />
+      </div>
       <img src="public/img/descarga.png" className="img-consttrction" alt="" />
     </main>
   );
--- src/pages/cities/Cities.jsx
+++ src/pages/cities/Cities.jsx
@@ -1,4 +1,4 @@
-import "./cities.css";
+/* import "./cities.css";
 import { useState, useEffect, useRef } from "react";
 import { getAllCities } from "../../services/cityService.js";
 import CardCities from "../../components/cardCities/CardCities.jsx";
@@ -41,6 +41,65 @@ const Cities = () => {
           <Anchor className="anchorCity" key={item._id} to="/cityDetail">
             <CardCities city={item} />
           </Anchor>
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default Cities;
+ */
+import "./cities.css";
+import { useState, useEffect, useRef } from "react";
+import { getAllCities } from "../../services/cityService.js";
+import CardCities from "../../components/cardCities/CardCities.jsx";
+import { Link as Anchor } from "react-router-dom";
+
+const Cities = () => {
+  const [cities, setCities] = useState([]);
+  const inputSearch = useRef(null);
+
+  useEffect(() => {
+    console.log("Fetching cities...");
+    getAllCities().then((data) => {
+      console.log("Cities data from getAllCities():", data);
+      setCities(data.response);
+    });
+  }, []);
+
+  const handleSearch = () => {
+    const search = inputSearch.current.value;
+    let query = `?`;
+    if (search) {
+      query += "name=" + search;
+    }
+    getAllCities(query).then((data) => setCities(data.response));
+  };
+
+  return (
+    <main>
+      <div>
+        <input
+          className="searchCity"
+          type="text"
+          placeholder="Search"
+          onInput={handleSearch}
+          ref={inputSearch}
+        />
+      </div>
+
+      <div className="container">
+        {cities.map((item) => (
+          <div key={item._id}>
+            <Anchor
+              className="anchorCity"
+              to={`/cityDetail?id=${item._id}&name=${encodeURIComponent(
+                item.name
+              )}&image=${encodeURIComponent(item.image)}`}
+            >
+              <CardCities city={item} />
+            </Anchor>
+          </div>
         ))}
       </div>
     </main>
