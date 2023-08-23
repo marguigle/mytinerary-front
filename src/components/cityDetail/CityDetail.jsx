c7b97919c4d49001fb664493458c0b31f26aaafa
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2023-08-23 13:52:58 -0300

    se agrega boton a cityDetail para retornar a cities

--- src/components/cityDetail/CityDetail.jsx
+++ src/components/cityDetail/CityDetail.jsx
@@ -1,19 +1,7 @@
-/* import "./cityDetail.css";
-
-const CityDetail = () => {
-  return (
-    <main className="main-city-detali">
-      <img src="public/img/descarga.png" className="img-consttrction" alt="" />
-    </main>
-  );
-};
-
-export default CityDetail;
- */
-
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
 import "./cityDetail.css";
+import { Link as Anchor } from "react-router-dom";
 
 const CityDetail = () => {
   const location = useLocation();
@@ -21,11 +9,18 @@ const CityDetail = () => {
 
   return (
     <main className="main-city-detali">
-      <div className="city-detail">
-        <h1>{name}</h1>
+      <div className="city-detail ">
+        <h3>{name}</h3>
         <img src={image} className="city-image" alt={name} />
+        <Anchor className="anchor-CityDetail" to={"../cities"}>
+          return to cities
+        </Anchor>{" "}
+        <img
+          src="public/img/descarga.png"
+          className="img-construction"
+          alt=""
+        />
       </div>
-      <img src="public/img/descarga.png" className="img-consttrction" alt="" />
     </main>
   );
 };
