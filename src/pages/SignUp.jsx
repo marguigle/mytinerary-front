1806ea87fae46f0e4ac2fd57805acab54db04718
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2023-09-11 12:56:57 -0300

    se crea formulario sign up

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,46 @@
+import "./signUp.css";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="form-container">
+      <form className="formulary">
+        <label className=" m-3">
+          {" "}
+          Name:
+          <input type="text" className="input" />
+        </label>
+
+        <label className="m-3">
+          {" "}
+          Email:
+          <input type="text" className="input" />
+        </label>
+
+        <label className="m-3">
+          {" "}
+          Photo:
+          <input type="text" className="input photo" />
+        </label>
+        <label className="m-3">
+          {" "}
+          Age:
+          <input type="text" className="input age" />
+        </label>
+
+        <label className="m-3">
+          {" "}
+          Password:
+          <input type="text" className="input password" />
+        </label>
+
+        <label className="m-3">
+          {" "}
+          Birdh Day:
+          <input type="text" className="input birdh-day" />
+        </label>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
