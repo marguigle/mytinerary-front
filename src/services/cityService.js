cbddf6586a94a72f5f6a557ff851c13ce54e6bb4
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-05-24 17:00:15 -0300

    se saca rewrite

--- src/services/cityService.js
+++ src/services/cityService.js
@@ -1,6 +1,14 @@
 import axios from "axios";
+import dotenv from "dotenv";
+
+// Cargar variables de entorno desde el archivo .env
+dotenv.config();
+
+// Obtener la URL base de las variables de entorno
+const baseURL = process.env.URL_BASE || "http://localhost:3000/api";
+
 const cityQueries = axios.create({
-  baseURL: "http://localhost:3000/api/cities",
+  baseURL: `${baseURL}/cities`, // Combinar la URL base con la ruta específica
 });
 
 export const getAllCities = async (queryParams = "") => {
@@ -11,9 +19,3 @@ export const getAllCities = async (queryParams = "") => {
     return [];
   }
 };
-
-/* cityQueries()
-.then((res) => {
-  setCities(res.data);
-})
-.catch((error) => console.log(error)); */
--- src/services/itineraryServivce.js
+++ src/services/itineraryServivce.js
@@ -1,6 +1,11 @@
 import axios from "axios";
+import dotenv from "dotenv";
+
+// Cargar variables de entorno desde el archivo .env
+dotenv.config();
+const baseURL = process.env.URL_BASE || "http://localhost:3000/api";
 const itineraryQueries = axios.create({
-  baseURL: "http://localhost:3000/api/itineraries",
+  baseURL: `${baseURL}/itineraries`, // Combinar la URL base con la ruta específica
 });
 
 export const getAllItineraries = async (queryParams = "") => {
