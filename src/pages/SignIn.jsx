ab9296e1c7e73ae24af52f3c36e7d329a04bac02
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-07-18 19:44:36 -0300

    se mejora sigin

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -55,36 +55,81 @@ const SignIn = () => {
     });
   };
   return (
-    <div className="form-container">
-      <form className="formulary" onSubmit={handleSubmit}>
-        <div className="lavel-input-cont">
-          <label className="">
-            {" "}
-            Email:
+    <>
+      {/* <form className="formulary" onSubmit={handleSubmit}>
+        <div className="form-container">
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlInput1" className="form-label">
+              Email address
+            </label>
             <input
               type="email"
-              name="email"
-              className="input email"
-              ref={email}
-              required
-            />
-          </label>
-        </div>
-        <div className="lavel-input-cont">
+              className="form-control"
+              id="exampleFormControlInput1"
+              placeholder="name@example.com"
+            ></input>
+          </div>
+          {/* <div className="lavel-input-cont">
           <label className="">
-            {" "}
-            Password:
-            <input
-              type="password"
-              name="password"
-              className="input password"
-              required
-              ref={password}
-            />
+          {" "}
+          Email:
+          <input
+          className="input email"
+          type="email"
+          name="email"
+          ref={email}
+          required
+          />
           </label>
+          </div> */}
+
+      {/* <div className="lavel-input-cont">
+          <label className="">
+          {" "}
+          Password:
+          <input
+          type="password"
+          name="password"
+          className="input password"
+          required
+          ref={password}
+          />
+          </label>
+          </div> 
+
+
+    
+      </form> */}
+
+      <form className="form-container" onSubmit={handleSubmit}>
+        <div className="mb-1">
+          <label htmlFor="exampleFormControlInput1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            name="email"
+            ref={email}
+            required
+            className="form-control  input-email"
+            id="exampleFormControlInput1"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput2" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            ref={password}
+            className="form-control input-password"
+            id="exampleFormControlInput2"
+          />
         </div>
 
         <button className="btn btn-secondary">sign in</button>
+
         <GoogleOAuthProvider clientId="820051858064-7lpsa7m8gg8opmj0c9i9qhddm8rikv2b.apps.googleusercontent.com">
           <GoogleLogin
             onSuccess={handleSubmitGoogle}
@@ -94,7 +139,7 @@ const SignIn = () => {
           />
         </GoogleOAuthProvider>
       </form>
-    </div>
+    </>
   );
 };
 
